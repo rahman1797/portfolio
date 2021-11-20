c411a90aec766cc1fd275f245459ef6564b4bfa7
Maulana Rahman Nur <maulanarahmannur67@gmail.com>
2021-11-20 22:39:22 +0700

    install and config firebase

---
+++ src/Config/Firebase/index.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDDkuNQSuAkna1zzWX2TqAWlKz6yMuEOPU",
+    authDomain: "portfolio-6a654.firebaseapp.com",
+    projectId: "portfolio-6a654",
+    storageBucket: "portfolio-6a654.appspot.com",
+    messagingSenderId: "975264144043",
+    appId: "1:975264144043:web:c378923bf995799e1af5ee",
+    measurementId: "G-S97LK6JWZR"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
